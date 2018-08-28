4c8cd7fdf85f936a29e464086dcc77da8258c402
Henrique Schmitt <schmitthenriq@gmail.com>
2018-08-28 16:06:30 -0300

    testes com inserts

--- Pessoa.py
+++ Pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.quant = quant
         self.salario = salario
         self.categoria = categoria
-        self.provFerias = None
+        self.provferias = None
         self.provdecimo = None
         self.fgts = None
         self.inss = None
@@ -17,11 +17,11 @@ class Pessoa:
     def calculoPessoal(self):
 
         if self.categoria != "diretor":
-            self.provFerias = (self.salario/12)*0.333333
+            self.provferias = (self.salario/12)*0.333333
             self.provdecimo = self.salario/12
-            self.fgts = (self.salario + self.provFerias + self.provdecimo) * 0.08
-            self.inss = (self.salario + self.provFerias + self.provdecimo) * 0.258
-            self.total = (self.salario + self.provdecimo + self.provFerias + self.fgts + self.inss) * self.quant #enquanto usarmos este meio de quantidade de pessoas na função usaremos esse quant aqui
+            self.fgts = (self.salario + self.provferias + self.provdecimo) * 0.08
+            self.inss = (self.salario + self.provferias + self.provdecimo) * 0.258
+            self.total = (self.salario + self.provdecimo + self.provferias + self.fgts + self.inss) * self.quant #enquanto usarmos este meio de quantidade de pessoas na função usaremos esse quant aqui
 
         else:
             self.inss = self.salario * 0.15
@@ -36,7 +36,7 @@ class Pessoa:
         cur.execute("""
         INSERT INTO pessoa (cargo, quant, salario, ferias, decimo, fgts, inss, total, categoria) 
         VALUES (?,?,?,?,?,?,?,?,?)
-        """, (self.cargo, self.quant, self.salario, self.ferias, self.decimo, self.fgts, self.inss, self.total, self.categoria))
+        """, (self.cargo, self.quant, self.salario, self.provferias, self.provdecimo, self.fgts, self.inss, self.total, self.categoria))
 
         cur.commit()
         conn.close()
